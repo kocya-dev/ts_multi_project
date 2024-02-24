dfeef73ca37a455295088a6cc37afcc612f49445
kohei.mochizuki <kohei.mochizuki@xacti-co.com>
2024-02-25 00:38:59 +0900

    カスタムマッチャー認識できない問題の解決try→失敗

--- jest.d.ts
+++ jest.d.ts
@@ -1,12 +1,18 @@
+import type { Assertion, AsymmetricMatchersContaining } from "vitest";
+
 export interface CustomMatchers<R = unknown> {
   toThrowTestError(a: any): R;
 }
 
 declare module "vitest" {
   namespace jest {
+    /*
     interface Expect extends CustomMatchers {}
     interface Matchers<R> extends CustomMatchers<R> {}
     interface InverseAsymmetricMatchers extends CustomMatchers {}
+    */
+    interface Assertion<T = any> extends CustomMatchers<T> {}
+    interface AsymmetricMatchersContaining extends CustomMatchers {}
   }
 }
 //export interface CustomMatchers<R = unknown> {
